e899279dc46fbd93018ceb8055970c196f89d13d
sou7226 <it0_s0u@icloud.com>
2024-08-21 11:11:45 +0900

    update: マップ移動に関するバグの修正

--- commands/map/map.js
+++ commands/map/map.js
@@ -4,90 +4,68 @@ const upMessage = ["u", "up", "↑", "上", "うえ", "ue", "う"];
 const rightMessage = ["r", "right", "→", "右", "みぎ", "migi", "み"];
 const leftMessage = ["l", "left", "←", "左", "ひだり", "hidari", "ひ"];
 const downMessage = ["d", "down", "↓", "下", "した", "sita", "し"];
-const { PrismaClient } = require('@prisma/client');
 require('dotenv').config();
-const prisma = new PrismaClient();
+
 async function map(message, mapInfo, playerInfo) {
     playerInfo.id = message.author.id
+    const mapWidth = 14;
+    const mapHeight = 9;
     let mapAttachment = await generateMap(mapInfo, playerInfo);
     const mapMessage = await message.channel.send({ files: [mapAttachment] });
     while (true) {
         try {
-            const collected = await message.channel
+            const collected = await message.channel 
                 .awaitMessages({
-                    filter: (m) => message.author.id === m.author.id,
-                    max: 1,
-                    time: 30_000,
-                    errors: ['time']
+                    filter: (m) => message.author.id === m.author.id, //messageはコマンド送信者のみに限定
+                    max: 1, //messageの数は一つまで
+                    time: 30_000, //時間は30秒
+                    errors: ['time'] //時間超過したらエラーを発生
                 });
             const responseMessage = collected.first();
-            if (responseMessage.content === "0") {
+            console.log(playerInfo.y)
+            if (responseMessage.content === "0") { //0なら終了
                 message.channel.send('終了します。');
                 break;
-            } else if (upMessage.includes(responseMessage.content)) {
-                await responseMessage.delete()
-                playerInfo.y--;
-                const isOutY = (playerInfo.y % 9) + 1
-                if (isOutY !== 0) {
+            } else if (upMessage.includes(responseMessage.content)) { //上がるメッセージを受信
+                await responseMessage.delete()//対象のメッセージを削除
+                if (playerInfo.y - 1 >= 0) { //yが0以上の場合(マップからはみ出さない場合)
+                    playerInfo.y--;
                     playerInfo = await migrate(playerInfo)
-                } else {
-                    try {
-                        const sentMessage = await message.channel.send("これ以上は進むことはできませんよ！");
-                        setTimeout(() => sentMessage.delete(), 3000); // 3秒後にメッセージを削除
-                    } catch (error) {
-                        console.error('メッセージの送信または削除に失敗しました:', error);
-                    }
+                    mapAttachment = await generateMap(mapInfo, playerInfo);
+                    mapMessage.edit({ files: [mapAttachment] })
+                } else {//0以下の場合(マップからはみ出る場合)
+                    await message.channel.send("これ以上は進むことはできませんよ！");
                 }
-                mapAttachment = await generateMap(mapInfo, playerInfo);
-                mapMessage.edit({ files: [mapAttachment] })
-            } else if (downMessage.includes(responseMessage.content)) {
+            } else if (downMessage.includes(responseMessage.content)) { //下がるメッセージを受信
                 await responseMessage.delete()
-                playerInfo.y++;
-                const isOutY = (playerInfo.y % 9) + 1
-                if (isOutY !== 0) {
+                if (playerInfo.y + 1 <= mapHeight) { //yが9以下の場合(マップからはみ出さない場合)
+                    playerInfo.y++;
                     playerInfo = await migrate(playerInfo)
-                } else {
-                    try {
-                        const sentMessage = await message.channel.send("これ以上は進むことはできませんよ！");
-                        setTimeout(() => sentMessage.delete(), 3000); // 3秒後にメッセージを削除
-                    } catch (error) {
-                        console.error('メッセージの送信または削除に失敗しました:', error);
-                    }
+                    mapAttachment = await generateMap(mapInfo, playerInfo);
+                    mapMessage.edit({ files: [mapAttachment] })
+                } else {//mapHeigh以上になる場合(マップからはみ出る場合)
+                    await message.channel.send("これ以上は進むことはできませんよ！");
                 }
-                mapAttachment = await generateMap(mapInfo, playerInfo);
-                mapMessage.edit({ files: [mapAttachment] })
             } else if (leftMessage.includes(responseMessage.content)) {
                 await responseMessage.delete()
-                playerInfo.x--;
-                const isOutX = (playerInfo.x % 14) + 1
-                if (isOutX !== 0) {
+                if (playerInfo.x - 1 >= 0) {
+                    playerInfo.x--;
                     playerInfo = await migrate(playerInfo)
-                } else {
-                    try {
-                        const sentMessage = await message.channel.send("これ以上は進むことはできませんよ！");
-                        setTimeout(() => sentMessage.delete(), 3000); // 3秒後にメッセージを削除
-                    } catch (error) {
-                        console.error('メッセージの送信または削除に失敗しました:', error);
-                    }
+                    mapAttachment = await generateMap(mapInfo, playerInfo);
+                    mapMessage.edit({ files: [mapAttachment] })
+                } else {//0以下の場合(マップからはみ出る場合)
+                    await message.channel.send("これ以上は進むことはできませんよ！");
                 }
-                mapAttachment = await generateMap(mapInfo, playerInfo);
-                mapMessage.edit({ files: [mapAttachment] })
             } else if (rightMessage.includes(responseMessage.content)) {
                 await responseMessage.delete()
-                playerInfo.x++;
-                const isOutX = (playerInfo.x % 14) + 1
-                if (isOutX !== 0) {
+                if (playerInfo.x + 1 <= mapWidth) {
+                    playerInfo.x++;
                     playerInfo = await migrate(playerInfo)
-                } else {
-                    try {
-                        const sentMessage = await message.channel.send("これ以上は進むことはできませんよ！");
-                        setTimeout(() => sentMessage.delete(), 3000); // 3秒後にメッセージを削除
-                    } catch (error) {
-                        console.error('メッセージの送信または削除に失敗しました:', error);
-                    }
+                    mapAttachment = await generateMap(mapInfo, playerInfo);
+                    mapMessage.edit({ files: [mapAttachment] })
+                } else {//mapWidth以上の場合(マップからはみ出る場合)
+                    await message.channel.send("これ以上は進むことはできませんよ！");
                 }
-                mapAttachment = await generateMap(mapInfo, playerInfo);
-                mapMessage.edit({ files: [mapAttachment] })
             }
         } catch (error) {
             return message.channel.send('時間切れです。');

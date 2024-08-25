1666df02ce9ef452e93a9cc6852c9aaff4ec8301
HashimotoKanna <63573845+HashimotoKanna@users.noreply.github.com>
2024-08-25 12:42:46 +0900

    update: メッセージ内容を変更

--- commands/battle/attackProcess/playerAttackProcess.js
+++ commands/battle/attackProcess/playerAttackProcess.js
@@ -1,7 +1,7 @@
 function playerAttackProcess(message, playerInfo, monsterInfo) {
     monsterInfo.hp = monsterInfo.hp - playerInfo.power
-    message.channel.send("```ansi\n[0;45mプレイヤーの攻撃！\n[0;34mモンスターに${playerInfo.power}のダメージ！\n[0;34mモンスターの残りHP:${monsterInfo.hp < 0 ? 0 : monsterInfo.hp}\n```")
+    message.channel.send(`\`\`\`ansi\n[0;45mプレイヤーの攻撃！\n[0;34mモンスターに${playerInfo.power}のダメージ！\n[0;34mモンスターの残りHP:${monsterInfo.hp < 0 ? 0 : monsterInfo.hp}\n\`\`\``)
 }
 module.exports = {
-    playerAttackProcess:playerAttackProcess
+    playerAttackProcess: playerAttackProcess
 }

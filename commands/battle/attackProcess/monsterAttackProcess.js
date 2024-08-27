8760a8b0015f4cd79d3b96f5a72bd01179e5db46
sou7226 <it0_s0u@icloud.com>
2024-08-28 03:35:11 +0900

    update: 文字化け修正

--- commands/battle/attackProcess/monsterAttackProcess.js
+++ commands/battle/attackProcess/monsterAttackProcess.js
@@ -1,6 +1,6 @@
 function monsterAttackProcess(message, playerInfo, monsterInfo) {
     playerInfo.hp = playerInfo.hp - monsterInfo.power
-    message.channel.send(`\`\`\`ansi\n[0;41m?????????\n[0;31m??????${monsterInfo.power}??????\n[0;31m????????HP:${playerInfo.hp < 0 ? 0 : playerInfo.hp}\n\`\`\``)
+    message.channel.send(`\`\`\`ansi\n[0;41m` + `モンスターの攻撃！\n`+`[0;31m`+`プレイヤーに${monsterInfo.power}のダメージ！\n`+`[0;31m`+`プレイヤーの残りHP:${playerInfo.hp < 0 ? 0 : playerInfo.hp}\n\`\`\``)
 }
 module.exports = {
     monsterAttackProcess: monsterAttackProcess
--- commands/battle/attackProcess/playerAttackProcess.js
+++ commands/battle/attackProcess/playerAttackProcess.js
@@ -1,6 +1,6 @@
 function playerAttackProcess(message, playerInfo, monsterInfo) {
     monsterInfo.hp = monsterInfo.hp - playerInfo.power
-    message.channel.send(`\`\`\`ansi\n[0;45m?????????\n[0;34m??????${playerInfo.power}??????\n[0;34m????????HP:${monsterInfo.hp < 0 ? 0 : monsterInfo.hp}\n\`\`\``)
+    message.channel.send(`\`\`\`ansi\n[0;45m`+`プレイヤーの攻撃\n`+`[0;34m`+`モンスターに${playerInfo.power}のダメージ！\n[0;34mプレイヤーの残りHP:${monsterInfo.hp < 0 ? 0 : monsterInfo.hp}\n\`\`\``)
 }
 module.exports = {
     playerAttackProcess: playerAttackProcess
